bef14a8b9306eac888590ef417a1df10b57fd731
Jakub Kozłowski <kubukoz@gmail.com>
2019-04-20 01:57:12 +0200

    Rewrite to tests(...)

--- tests/src/main/scala/flawless/tests/GetStatsTest.scala
+++ tests/src/main/scala/flawless/tests/GetStatsTest.scala
@@ -8,36 +8,40 @@ object GetStatsTest extends PureSuite {
   import flawless.syntax.pure._
   import RunStats.Stat
 
-  val runSuitePure: PureTest[SuiteResult] = test("1 test: 1s/0f") {
-    val input = test("Example") {
-      (1 shouldBe 1)
-    }.run(TestRun(Nil, Nil))
+  val runSuitePure: PureTest[SuiteResult] = tests(
+    test("1 test: 1s/0f") {
+      val input = test("Example") {
+        (1 shouldBe 1)
+      }.run(TestRun(Nil, Nil))
 
-    getStats(NonEmptyList.of(input)) shouldBe RunStats(
-      Stat(1, 1, 0),
-      Stat(1, 1, 0),
-      Stat(1, 1, 0)
-    )
-  } |+| test("1 test: 1s/1f") {
-    val input = test("Example") {
-      (1 shouldBe 1) |+| (1 shouldBe 2)
-    }.run(TestRun(Nil, Nil))
+      getStats(NonEmptyList.of(input)) shouldBe RunStats(
+        Stat(1, 1, 0),
+        Stat(1, 1, 0),
+        Stat(1, 1, 0)
+      )
+    },
+    test("1 test: 1s/1f") {
+      val input = test("Example") {
+        (1 shouldBe 1) |+| (1 shouldBe 2)
+      }.run(TestRun(Nil, Nil))
 
-    getStats(NonEmptyList.of(input)) shouldBe RunStats(
-      Stat(1, 0, 1),
-      Stat(1, 0, 1),
-      Stat(2, 1, 1)
-    )
-  } |+| test("1 test: 1s/2f") {
-    val input = test("Example") {
-      (1 shouldBe 1) |+| (1 shouldBe 2).combineN(2)
-    }.run(TestRun(Nil, Nil))
+      getStats(NonEmptyList.of(input)) shouldBe RunStats(
+        Stat(1, 0, 1),
+        Stat(1, 0, 1),
+        Stat(2, 1, 1)
+      )
+    },
+    test("1 test: 1s/2f") {
+      val input = test("Example") {
+        (1 shouldBe 1) |+| (1 shouldBe 2).combineN(2)
+      }.run(TestRun(Nil, Nil))
 
-    getStats(NonEmptyList.of(input)) shouldBe RunStats(
-      Stat(1, 0, 1),
-      Stat(1, 0, 1),
-      Stat(3, 1, 2)
-    )
-  }
+      getStats(NonEmptyList.of(input)) shouldBe RunStats(
+        Stat(1, 0, 1),
+        Stat(1, 0, 1),
+        Stat(3, 1, 2)
+      )
+    }
+  )
   //todo property based tests - the total amount of each stat should be the sum of failed+succeeded
 }
