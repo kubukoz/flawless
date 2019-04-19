5ce42849e5a9942fb054568b5824b553891cc249
Jakub Kozłowski <kubukoz@gmail.com>
2019-04-20 01:55:24 +0200

    Add test

--- tests/src/main/scala/flawless/tests/GetStatsTest.scala
+++ tests/src/main/scala/flawless/tests/GetStatsTest.scala
@@ -28,5 +28,16 @@ object GetStatsTest extends PureSuite {
       Stat(1, 0, 1),
       Stat(2, 1, 1)
     )
+  } |+| test("1 test: 1s/2f") {
+    val input = test("Example") {
+      (1 shouldBe 1) |+| (1 shouldBe 2).combineN(2)
+    }.run(TestRun(Nil, Nil))
+
+    getStats(NonEmptyList.of(input)) shouldBe RunStats(
+      Stat(1, 0, 1),
+      Stat(1, 0, 1),
+      Stat(3, 1, 2)
+    )
   }
+  //todo property based tests - the total amount of each stat should be the sum of failed+succeeded
 }
