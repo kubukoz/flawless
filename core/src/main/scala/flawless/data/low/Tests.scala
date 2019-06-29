097c960d8539d089b3bd0f6bda1dfe017735517c
Jakub Kozłowski <kubukoz@gmail.com>
2019-06-29 20:58:56 +0200

    Rename Both to Merge, make it hold a NEC

--- core/src/main/scala/flawless/data/low/Tests.scala
+++ core/src/main/scala/flawless/data/low/Tests.scala
@@ -6,6 +6,7 @@ import cats.implicits._
 import cats.tagless.FunctorK
 import cats.Functor
 import cats.Semigroup
+import cats.data.NonEmptyChain
 import cats.~>
 import flawless.SuiteResult
 import flawless.TTest
@@ -21,20 +22,23 @@ object Tests {
 
   final case class Pure(result: SuiteResult) extends Tests[Nothing, SuiteResult]
   final case class Run(iotest: IO[SuiteResult]) extends Tests[Nothing, SuiteResult]
-  final case class Both[F[_], A](left: F[A], right: F[A], sem: Semigroup[A]) extends Tests[F, A]
   final case class LiftResource[F[_], A, B](resource: Resource[IO, A], f: A => F[B]) extends Tests[F, B]
+  //todo merge these two? add new type parameter to sequence and rename it to merge
+  final case class Merge[F[_], A](tests: NonEmptyChain[F[A]], sem: Semigroup[A]) extends Tests[F, A]
   final case class Sequence[F[_], S[_], A](tests: S[F[A]], merge: S[IO[A]] => IO[S[A]], functor: Functor[S]) extends Tests[F, S[A]]
+  final case class Map[F[_], A, B](test: F[A], f: A => B) extends Tests[F, B]
 
   object algebras {
 
     val interpret: HAlgebra[Tests, IO] = new HAlgebra[Tests, IO] {
 
       def apply[A](fa: Tests[IO, A]): IO[A] = fa match {
-        case Pure(result)                                  => IO.pure(result)
-        case Run(io)                                       => io
-        case Both(left, right, implicit0(s: Semigroup[A])) => left |+| right
-        case LiftResource(r, use)                          => r.use(use)
-        case Sequence(tests, merge, _)                     => merge(tests)
+        case Pure(result)                             => IO.pure(result)
+        case Run(io)                                  => io
+        case m: Map[IO, a, A]                         => m.test.map(m.f)
+        case Merge(tests, implicit0(s: Semigroup[A])) => tests.reduce
+        case LiftResource(r, use)                     => r.use(use)
+        case Sequence(tests, merge, _)                => merge(tests)
       }
     }
 
@@ -47,16 +51,23 @@ object Tests {
         }
       }
 
-    type JustString[A] = String
+    type IOString[_] = IO[String]
 
-    val show: algebra.HAlgebra[Tests, JustString] = new algebra.HAlgebra[Tests, JustString] {
+    val show: algebra.HAlgebra[Tests, IOString] = new algebra.HAlgebra[Tests, IOString] {
 
-      def apply[A](fa: Tests[JustString, A]): String = fa match {
-        case Pure(result)          => s"result($result)"
-        case Run(_)                => "iotest"
-        case Both(left, right, _)  => s"Both($left |+| $right)"
-        case LiftResource(_, _)    => s"LiftResource(<resource>, <function>)"
-        case Sequence(tests, _, _) => s"Sequence($tests, <function>)"
+      def apply[A](fa: Tests[IOString, A]): IO[String] = fa match {
+        case Pure(result) => show"result($result)".pure[IO]
+        case Run(test)    => test.map(result => show"Run($result)")
+        case Merge(elems, _) =>
+          (elems: NonEmptyChain[IO[String]]).sequence.map { results =>
+            results.mkString_("Merge(", " |+| ", ")")
+          }
+        case Map(test, _) =>
+          test.map { r =>
+            show"Map($r, <function>)"
+          }
+        case LiftResource(_, _)    => show"LiftResource(<resource>, <function>)".pure[IO]
+        case Sequence(tests, _, _) => s"Sequence($tests, <function>)".pure[IO]
       }
     }
   }
@@ -64,11 +75,12 @@ object Tests {
   implicit def testsFunctorK[A]: FunctorK[Tests[?[_], A]] = new FunctorK[Tests[?[_], A]] {
 
     def mapK[F[_], G[_]](af: Tests[F, A])(fk: F ~> G): Tests[G, A] = af match {
-      case Pure(result)             => Pure(result)
-      case Run(iotest)              => Run(iotest)
-      case b: Both[F, a]            => Both[G, a](fk(b.left), fk(b.right), b.sem)
+      case p: Pure                  => p
+      case r: Run                   => r
+      case m: Map[F, a, A]          => Map(fk(m.test), m.f)
+      case m: Merge[F, a]           => Merge[G, a](m.tests.map(fk(_)), m.sem)
       case ss: Sequence[_, s, a]    => Sequence[G, s, a](ss.functor.map(ss.tests)(fk(_)), ss.merge, ss.functor)
-      case l: LiftResource[_, a, b] => LiftResource[G, a, b](l.resource, a => fk(l.f(a)))
+      case l: LiftResource[F, a, b] => LiftResource[G, a, b](l.resource, a => fk(l.f(a)))
     }
   }
 
