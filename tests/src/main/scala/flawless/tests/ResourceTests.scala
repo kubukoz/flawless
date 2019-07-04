559c89baf9c13835d0d1f476037a8f80050089e5
Jakub Kozłowski <kubukoz@gmail.com>
2019-07-05 00:10:15 +0100

    Refactoring

--- tests/src/main/scala/flawless/tests/ResourceTests.scala
+++ tests/src/main/scala/flawless/tests/ResourceTests.scala
@@ -23,36 +23,41 @@ class ResourceTests(implicit cs: ContextShift[IO]) extends Suite {
       (test, resourceFinalized.get)
     }
 
-  val runSuite: Tests[SuiteResult] = tests(
-    test("Tests.resource cleans up and forwards error on errors in use") {
-      val e = new Throwable("oops")
+  val testFailure = test("Tests.resource cleans up and forwards error on errors in use") {
+    val e = new Throwable("oops")
 
-      mkTest(IO.raiseError(e)).flatMap {
-        case (test, resourceFinalized) =>
-          test.attempt *> resourceFinalized
-      }.map(_.shouldBe(ExitCase.error(e)))
-    },
-    test("Tests.resource cleans up and forwards cancelation on cancelation in use") {
-      Deferred[IO, Unit].flatMap {
-        testStarted =>
-          Deferred[IO, ExitCase[Throwable]].flatMap {
-            testFinalized =>
-              val nonTerminatingTest = mkTest {
-                testStarted.complete(()).bracketCase(_ => IO.never)((_, ec) => testFinalized.complete(ec))
-              }
+    mkTest(IO.raiseError(e)).flatMap {
+      case (test, resourceFinalized) =>
+        (test.attempt, resourceFinalized).tupled
+    }.map {
+      case (testResult, resourceEC) =>
+        testResult.shouldBe(Left(e)) |+| resourceEC.shouldBe(ExitCase.error(e))
+    }
+  }
 
-              val runTest = nonTerminatingTest.flatMap {
-                case (test, resourceFinalized) =>
-                  //run test, wait until it starts, then cancel and wait for finalization
-                  test.start.flatMap { testStarted.get *> _.cancel } *> resourceFinalized
-              }
+  val testCancelation = test("Tests.resource cleans up and forwards cancelation on cancelation in use") {
+    Deferred[IO, Unit].flatMap { testStarted =>
+      Deferred[IO, ExitCase[Throwable]].flatMap { testFinalized =>
+        val nonTerminatingTest = mkTest {
+          testStarted.complete(()).bracketCase(_ => IO.never)((_, ec) => testFinalized.complete(ec))
+        }
 
-              (runTest, testFinalized.get).mapN {
-                case (resourceEC, testEC) =>
-                  resourceEC.shouldBe(ExitCase.canceled) |+| testEC.shouldBe(ExitCase.canceled)
-              }
-          }
+        val runTest = nonTerminatingTest.flatMap {
+          case (test, resourceFinalized) =>
+            //run test, wait until it starts, then cancel and wait for finalization
+            test.start.flatMap { testStarted.get *> _.cancel } *> resourceFinalized
+        }
+
+        (runTest, testFinalized.get).mapN {
+          case (resourceEC, testEC) =>
+            resourceEC.shouldBe(ExitCase.canceled) |+| testEC.shouldBe(ExitCase.canceled)
+        }
       }
     }
+  }
+
+  val runSuite: Tests[SuiteResult] = tests(
+    testFailure,
+    testCancelation
   )
 }
