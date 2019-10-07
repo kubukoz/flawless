350b07aff99f15b16c99ae80bf7a716ea59414c2
Jakub Kozłowski <kubukoz@gmail.com>
2019-10-07 14:55:53 +0200

    Use new kind-projector syntax

--- examples/src/main/scala/flawless/examples/ExampleTests.scala
+++ examples/src/main/scala/flawless/examples/ExampleTests.scala
@@ -3,6 +3,7 @@ package flawless.examples
 //here goes the demo!
 
 import cats.data.NonEmptyList
+import cats.data.OptionT
 import cats.effect.ExitCode
 import cats.effect.IO
 import cats.effect.IOApp
@@ -11,42 +12,45 @@ import flawless._
 import flawless.examples.doobie.DoobieQueryTests
 import _root_.doobie.util.ExecutionContexts
 import _root_.doobie.hikari.HikariTransactor
+import cats.Foldable
 import cats.effect.Blocker
 import flawless.data.neu.TestApp
 import cats.effect.Resource
+import fs2.Pipe
+import cats.implicits._
 
 object ExampleTests extends IOApp with TestApp {
-  /*
+
   //flaky test detector
-  def deflake(maxRetries: Option[Long] = Some(10000L)): IO[SuiteResult] => IO[SuiteResult] =
-    io => {
+  def deflake(maxRetries: Option[Long] = Some(10000L)): Test[IO] => Test[IO] =
+    _.via {
       def counted[A]: Pipe[IO, A, A] = maxRetries match {
         case Some(max) => _.take(max)
         case None      => identity
       }
+      def successful[F[_]: Foldable](result: F[Assertion]): Boolean = result.forall(_.isSuccessful)
 
-      val findFirstFailure = fs2.Stream
-        .repeatEval(io)
-        .through(counted)
-        .zipWithIndex
-        .find {
-          case (suite, _) => suite.isFailed
-        }
-        .compile
-        .last
-        .flatMap {
-          case Some((failure, failureIndex)) =>
-            putStrLn(show"Suite failed after ${failureIndex + 1} successes").as(failure.some)
-          case None =>
-            putStrLn(show"Didn't find flaky suite after $maxRetries successes").as(none)
-        }
+      _.via { io =>
+        val findFirstFailure = fs2.Stream
+          .repeatEval(io)
+          .through(counted)
+          .zipWithIndex
+          .find {
+            case (result, _) => !successful(result)
+          }
+          .compile
+          .last
+          .flatMap {
+            case Some((failure, failureIndex)) => console.putStrLn(show"Suite failed after ${failureIndex + 1} successes").as(failure.some)
+            case None                          => console.putStrLn(show"Didn't find flaky suite after $maxRetries successes").as(none)
+          }
 
-      io.flatMap { firstResult =>
-        if (!firstResult.isSuccessful) firstResult.pure[IO]
-        else OptionT(findFirstFailure).getOrElse(firstResult)
+        io.flatMap { firstResult =>
+          if (!successful(firstResult)) firstResult.pure[IO]
+          else OptionT(findFirstFailure).getOrElse(firstResult)
+        }
       }
     }
-   */
 
   val sequentialTests = NonEmptyList.of(
     FirstSuite,
@@ -71,7 +75,7 @@ object ExampleTests extends IOApp with TestApp {
         connectEc,
         blocker
       )
-    } yield NonEmptyList.fromListUnsafe(List.fill(10)(new DoobieQueryTests(transactor).runSuite.toSuites))
+    } yield NonEmptyList.of(new DoobieQueryTests(transactor).runSuite.toSuites).combineN(10)
   }
 
   val runSequentials = Suites.sequential(
@@ -90,6 +94,6 @@ object ExampleTests extends IOApp with TestApp {
 
   override def run(args: List[String]): IO[ExitCode] =
     runTests(args)(
-      testRange //.via(deflake()).via(modifiers.catching)
+      testRange //.viaTest(deflake()) //.via(modifiers.catching)
     )
 }
