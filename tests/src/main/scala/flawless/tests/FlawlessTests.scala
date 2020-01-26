3ba8e82c60c31216f81ed2d807311d99e999b8cd
Jakub Kozłowski <kubukoz@gmail.com>
2020-01-26 14:17:48 +0100

    Add tagless test

--- tests/src/main/scala/flawless/tests/FlawlessTests.scala
+++ tests/src/main/scala/flawless/tests/FlawlessTests.scala
@@ -10,10 +10,14 @@ import flawless.data.Suites
 //test runner for the whole module
 object FlawlessTests extends IOApp with TestApp {
 
-  def run(args: List[String]): IO[ExitCode] = runTests[IO](args) {
-    Suites.sequential(
-      GetStatsTest.runSuite.toSuites,
-      ReporterTest.runSuite.toSuites
-    )
-  }
+  def run(args: List[String]): IO[ExitCode] =
+    MyAlg.syncInstance[IO].flatMap { implicit alg =>
+      runTests[IO](args) {
+        Suites.sequential(
+          GetStatsTest.runSuite.toSuites,
+          ReporterTest.runSuite.toSuites,
+          new TaglessTest[IO].runSuite.toSuites
+        )
+      }
+    }
 }
---
+++ tests/src/main/scala/flawless/tests/TaglessTest.scala
@@ -0,0 +1,43 @@
+package flawless.tests
+
+import flawless.SuiteClass
+import flawless.data.Suite
+import flawless.dsl._
+import cats.tagless.finalAlg
+import cats.effect.Sync
+import cats.effect.concurrent.Ref
+import cats.implicits._
+import cats.data.State
+import cats.FlatMap
+
+@finalAlg
+trait MyAlg[F[_]] {
+  def reset: F[Unit]
+  def hello: F[Int]
+}
+
+object MyAlg {
+
+  def syncInstance[F[_]: Sync]: F[MyAlg[F]] = Ref[F].of(0).map { ref =>
+    new MyAlg[F] {
+      val hello: F[Int] = ref.modifyState(
+        State.modify[Int](_ + 1) *> State.get
+      )
+
+      val reset: F[Unit] = ref.set(0)
+    }
+  }
+}
+
+final class TaglessTest[F[_]: MyAlg: FlatMap] extends SuiteClass[F] {
+
+  val runSuite: Suite[F] = suite("tagless") {
+    tests(
+      test("first tagless")(
+        (MyAlg[F].reset *> MyAlg[F].hello, MyAlg[F].hello).mapN { (before, after) =>
+          ensureEqual(before, 1) |+| ensureEqual(after, 2)
+        }
+      )
+    )
+  }
+}
